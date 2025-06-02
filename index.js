2599694bcb75242354a107c3b8b986f8d342b555
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-06-02 15:10:43 +0530

    upadte

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.get("/weather", (req, res) => {
 
 app.get("/products", (req, res) => {
   const products = [
-    { name: "Product 1", price: 34 },
-    { name: "Product 2", price: 64 },
-    { name: "Product 3", price: 45 },
+    { name: "Mobile", price: 34 },
+    { name: "Tablet", price: 64 },
+    { name: "Laptop", price: 45 },
   ];
   res.json(products);
 });
