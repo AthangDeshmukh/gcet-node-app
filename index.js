bbbca7dbff03f2e50732c41bbf874fca5751d364
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-06-04 14:33:58 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from 'dotenv';
-
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
@@ -19,12 +18,6 @@ app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/orders",orderRouter)
 
-app.get("/", (req, res) => {
-  return res.send("Hello, World! This is the API Index.");
-  })
-
-
-
 mongoose
   .connect(MONGO_URI)
   .then(() => {
@@ -35,74 +28,3 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
-
-
-// 
-// 
-//  import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-
-// import userRouter from "./routes/userRoutes.js";
-// import productRouter from "./routes/productRoutes.js";
-// import orderRouter from "./routes/ordersRoutes.js";
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// const MONGO_URI = process.env.MONGO_URI
-
-// app.use("/users", userRouter);
-// app.use("/products", productRouter);
-// app.use("/orders",orderRouter)
-
-// mongoose
-//   .connect(MONGO_URI)
-//   .then(() => {
-//     app.listen(8080, () => {
-//       console.log("Server Started on port 8080");
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-  
-//app.listen(8080, () => {
-//   mongoose.connect(`${MONGODB_URI}`);
-//   console.log("Server Started");
-// });
-
-//testing
-
-/**app.get("/", (req, res)=>{
-  return res.send(`<h1>Welcome to the API Index</h1>
-    <ol>
-      <li><a href="/greet">/greet</a></li>
-      <li><a href="/name">/name</a></li>
-      <li><a href="/weather">/weather</a></li>
-      <li><a href="/products">/products</a></li>
-      <li><a href="/register">/register</a></li>
-      <li><a href="/login">/login</a></li>
-    </ol>
-  `);
-   res.send("Good Morning!!");
-});
-
-app.get("/greet", (req, res)=>{
-  res.send("Greetings!!");
-} );
-
-app.get("/name", (req, res)=>{
-  res.send("hyeyy");
-} );
-
-app.get("/weather", (req, res)=>{
-  res.send("29 degrees");
-});
-
-**/
--- models/ordersModel.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-const ordersSchema = mongoose.Schema({
-  email: {type:String},
-  price: {type:String},
-});
-
-export default mongoose.model("Orders", ordersSchema);
