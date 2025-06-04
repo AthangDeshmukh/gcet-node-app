320da4e3796f079e8ed7116c28c1f5b90f265b3d
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-06-04 12:42:22 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from 'dotenv';
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
-import orderRouter from "./routes/ordersRoutes.js";
-import dotenv from 'dotenv';
-dotenv.config();
+import orderRouter from "./routes/orderRoutes.js";
 
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -29,6 +28,40 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+// 
+// 
+//  import express from "express";
+// import mongoose from "mongoose";
+// import cors from "cors";
+
+// import userRouter from "./routes/userRoutes.js";
+// import productRouter from "./routes/productRoutes.js";
+// import orderRouter from "./routes/ordersRoutes.js";
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// const MONGO_URI = process.env.MONGO_URI
+
+// app.use("/users", userRouter);
+// app.use("/products", productRouter);
+// app.use("/orders",orderRouter)
+
+// mongoose
+//   .connect(MONGO_URI)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server Started on port 8080");
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+  
 //app.listen(8080, () => {
 //   mongoose.connect(`${MONGODB_URI}`);
 //   console.log("Server Started");
