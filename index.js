942dcd4eea53071a134b33557eedcf77375ee3e8
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-06-04 11:57:06 +0530

    update

--- index.js
+++ index.js
@@ -1,41 +1,63 @@
 import express from "express";
 import mongoose from "mongoose";
-// import dotenv from "dotenv";
 import cors from "cors";
-// dotenv.config();
-import ordersRouter from "./routes/ordersRoutes.js";
+
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
-// const MONGOCB_URI = process.env.MONGOCB_URI 
-// import userRouter from "./routes/userRoutes.js";
-// import productRouter from "./routes/productRoutes.js";
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/users", userRouter);
-
-// app.use("/products", productRouter);
-
-// app.listen(8080, () => {
-//   mongoose.connect("mongodb://localhost:27017/gcet");
-//   console.log("Server Started");
-// });
-
-
+import orderRouter from "./routes/orderRoutes.js";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const MONGO_URI = process.env.MONGO_URI
+
 app.use("/users", userRouter);
-
 app.use("/products", productRouter);
+app.use("/orders",orderRouter)
 
-app.use("/orders", ordersRouter);
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+//app.listen(8080, () => {
+//   mongoose.connect(`${MONGODB_URI}`);
+//   console.log("Server Started");
+// });
 
-app.listen(8080, () => {
-  mongoose.connect("mongodb://localhost:27017/gcet");
-  console.log("Server Started");
+
+/**app.get("/", (req, res)=>{
+  return res.send(`<h1>Welcome to the API Index</h1>
+    <ol>
+      <li><a href="/greet">/greet</a></li>
+      <li><a href="/name">/name</a></li>
+      <li><a href="/weather">/weather</a></li>
+      <li><a href="/products">/products</a></li>
+      <li><a href="/register">/register</a></li>
+      <li><a href="/login">/login</a></li>
+    </ol>
+  `);
+   res.send("Good Morning!!");
 });
+
+app.get("/greet", (req, res)=>{
+  res.send("Greetings!!");
+} );
+
+app.get("/name", (req, res)=>{
+  res.send("hyeyy");
+} );
+
+app.get("/weather", (req, res)=>{
+  res.send("29 degrees");
+});
+
+**/
